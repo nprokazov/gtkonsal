9f5b0564619553720fea8f15442a17df5652c75d
Igor Bobko <iibobko@gmail.com>
2020-02-13 23:51:48 +0300

    [GTK-4]. Init.

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Customer />
+    <footer class="info">
+      <p>Based on a project written by <a href="http://evanyou.me">Evan You</a></p>
+      <p>Original Vue TodoApp project is <a href="https://vuejs.org/v2/examples/todomvc.html">here</a></p>
+      <p>Modified for this tutorial by Andrew Hughes</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+  import Customer from './components/Customer'
+  // app Vue instance
+  const app = {
+    name: 'app',
+    components: {
+      Customer
+    },
+    // app initial state
+    data: () => {
+      return {
+      }
+    }
+  }
+
+  export default app
+</script>
+
+<style>
+  [v-cloak] { display: none; }
+</style>
