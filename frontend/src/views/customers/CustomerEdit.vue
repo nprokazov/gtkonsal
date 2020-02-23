19e012aebcd90981e410db1ab472b53625bbff46
Николай <nkpro@ya.ru>
2020-02-23 18:25:02 +0300

    GTK-12. Added customer Box.

--- frontend/src/views/customers/CustomerEdit.vue
+++ frontend/src/views/customers/CustomerEdit.vue
@@ -1,9 +1,12 @@
 <template>
     <section class="py-5 z-depth-1 px-md-5 dark-grey-text">
-        <label for="customer_name">Наименование клиента</label>
-        <input class="form-control" id="customer_name" type="text">
-
-        <mdb-btn @click="save" color="primary">Сохранить</mdb-btn>
+        <p class="h4 mb-4">Новый клиент</p>
+        <input class="form-control" id="customer_name" type="text" placeholder="Наименование клиента">
+        <input class="form-control" id="customer_cargo" type="text" placeholder="Перевозимый груз">
+        <input class="form-control" id="customer_email" type="email" placeholder="E-mail">
+        <input class="form-control" id="customer_Phone" type="tel"  placeholder="Номер телефона" >
+        <input class="form-control" id="customer_Notes" type="text"  placeholder="Примечания" >
+        <mdb-btn @click="save" color="primary">Добавить</mdb-btn>
     </section>
 </template>
 
